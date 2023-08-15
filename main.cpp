7a8549ca925c05f593effb0e69f56c8ffbc686c6
_Marian <93159563+pixel0000@users.noreply.github.com>
2023-08-15 21:07:13 +0300

    Update main.cpp
    -deleted 2 line

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 double power(double base, int exponent);
 int suma (int a,int b);
 
-
 int main() {
   //apelarea functiei
 int suma1=suma (2,3);
@@ -15,7 +14,6 @@ cout <<suma1<<endl;
 cout <<pow(2,3)<<endl; 
 cout <<power(2,3)<<endl;//parametrii
 }
-
 double power(double base, int exponent){ //argumente
 double result=1;
 for(int i=0;i<exponent;i++){
